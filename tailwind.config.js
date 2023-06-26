c6e63b00c009765a9f92d0e786a0468daf4323b0
dLevz <100078678+dLevz@users.noreply.github.com>
2023-06-26 11:46:44 +0100

    feat: nav hamburger menu at xs viewport

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,6 +9,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'xs': '375px',
+        ...defaultTheme.screens,
+      },
       fontFamily: {
 				raleway: ["var(--font-raleway)"]
 			},
